ec2d8143d53f2e9bca88b35ebe71150c00a0f10d
Vinicius Christiano <vinee_urso@hotmail.com>
2026-02-03 11:17:47 -0300

    Projeto sistema de notas

---
+++ README.md
@@ -0,0 +1,23 @@
+# Sistema de Notas em JavaScript
+
+Projeto criado para praticar:
+
+- Funções
+- Condicionais (if/else)
+- Validação de dados
+
+## Funcionalidade
+
+O programa calcula a média de três notas e informa se o aluno foi:
+
+- Aprovado
+- Em recuperação
+- Reprovado
+
+## Tecnologias
+
+- JavaScript (Node.js)
+
+## Aprendizado
+
+Neste projeto aprendi a organizar funções e validar dados.
---
+++ index.js
@@ -0,0 +1,29 @@
+//? ---- INSTRUÇÕES:
+//* 1. Receber 3 notas
+//* 2. calcular media
+//* 3. imprimir aprovado/recuperação/reprovado
+//TODO conceitos: --- if/else, number, variáveis
+
+function calcMedia(nota1, nota2, nota3) {
+    if (nota1 < 0 || nota2 < 0 || nota3 < 0) {
+        return "Notas inválidas";
+    }
+
+    const media = (nota1 + nota2 + nota3) / 3;
+
+    if (media >= 6) {
+        return `Aluno aprovado, a média foi: ${media.toFixed(2)}`;
+    } else if (media >= 5) {
+        return `Aluno em recuperação, a média foi: ${media.toFixed(2)}`;
+    } else {
+        return `Aluno reprovado, a média foi: ${media.toFixed(2)}`;
+    }
+}
+
+let nota1 = 3.6;
+let nota2 = 4.8;
+let nota3 = 8.4;
+
+const resultado = calcMedia(nota1, nota2, nota3);
+
+console.log(resultado);
